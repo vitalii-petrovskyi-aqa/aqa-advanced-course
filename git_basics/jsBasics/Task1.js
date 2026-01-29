597abd4866d5c1444ffc400e40a7c68b2a24eccc
Vitalii Petrovskyi <vitalii.petrovskyi@mgid.com>
2026-01-29 15:02:54 +0200

    home task from lesson 4

---
+++ git_basics/jsBasics/Task1.js
@@ -0,0 +1,15 @@
+let str = `randomTextexample`;
+console.log(`1. String: ${str}\n\ttype: ${typeof str}`);
+
+let number = 27012026;
+console.log(`2. Number: ${number}\n\ttype: ${typeof number}`);
+
+let bool1 = true;
+let bool2 = false;
+console.log(`3. Booleans: ${bool1} and ${bool2}\n\ttypes: ${typeof bool1} and ${typeof bool1}`);
+
+let testNull = null;
+console.log(`4. Null: ${testNull}\n\ttype: ${typeof testNull}`);
+
+let testUndefined;
+console.log(`5. Undefined: ${typeof testUndefined}\n\ttype: ${typeof testUndefined}`);
---
+++ git_basics/jsBasics/Task4_2.js
@@ -0,0 +1,5 @@
+let lengthRoom=4.65;
+let widthRoom=3.5;
+let roomArea= widthRoom * widthRoom;
+
+console.log(roomArea.toFixed(2))

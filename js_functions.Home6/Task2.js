028b492be7ee83b2f7b36481c76b7b443cd41fc6
Vitalii Petrovskyi <vitalii.petrovskyi@mgid.com>
2026-01-31 17:17:49 +0200

    Home task 6. Added one more case for task2

--- js_functions.Home6/Task2.js
+++ js_functions.Home6/Task2.js
@@ -2,3 +2,13 @@ const adultUser = (ageUser) => ageUser >= 18 ? true : false
 
 console.log(adultUser(15))
 console.log(adultUser(25))
+
+const isAdult = (age) => {
+	if (age >= 18) {
+		return true;
+	}
+	return false;
+};
+
+console.log(isAdult(15));
+console.log(isAdult(25));
